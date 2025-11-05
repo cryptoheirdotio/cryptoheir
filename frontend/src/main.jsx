f9f8ceca3186d6d41d46b8c7e7a6b723481d6e32
Ethan Zhang <yzhang.wa@gmail.com>
2025-11-05 16:33:01 -0600

    configure wagmi and rainbowkit providers

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { WagmiProvider } from 'wagmi';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+
+import { config } from './config/wagmi.js';
+import './index.css';
+import '@rainbow-me/rainbowkit/styles.css';
+import App from './App.jsx';
+
+// Create a client for React Query
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <App />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   </StrictMode>,
 )
