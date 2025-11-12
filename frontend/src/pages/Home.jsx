17ee237d9255cb06999ff0675b899fb8b570d22d
Ethan Zhang <yzhang.wa@gmail.com>
2025-11-12 12:12:49 -0600

    migrate DepositForm to useWriteContract

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -13,7 +13,7 @@ export function Home() {
         </div>
       </div>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-        <DepositForm contract={contract} account={account} />
+        <DepositForm account={account} />
         <InheritanceManager account={account} />
       </div>
     </>
