c90eee898d9a6e08319e4a3d4d3aae2e3a1a95b7
Ethan Zhang <yzhang.wa@gmail.com>
2025-12-25 01:28:55 -0600

    remove a duplicate warning message

--- frontend/src/components/DepositForm/AlertMessages.jsx
+++ frontend/src/components/DepositForm/AlertMessages.jsx
@@ -1,14 +1,6 @@
 export const AlertMessages = ({ error, success, tokenType, needsApproval, tokenAddress }) => {
   return (
     <>
-      {tokenType === 'erc20' && !needsApproval && tokenAddress && (
-        <div className="alert alert-info shadow-smooth">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-current shrink-0 w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-          </svg>
-          <span className="font-medium">Token approved! You can now deposit.</span>
-        </div>
-      )}
       {error && (
         <div className="alert alert-error mt-6 shadow-smooth">
           <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
